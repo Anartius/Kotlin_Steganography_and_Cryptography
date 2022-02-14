1a1b47283b858cc77ee733c0e1058e65d95cc4c4
Anatoliy <anartius@gmail.com>
2022-02-14 10:23:51 +0100

    Completion of the first stage.

--- Steganography and Cryptography/task/src/cryptography/Main.kt
+++ Steganography and Cryptography/task/src/cryptography/Main.kt
@@ -1,6 +1,17 @@
 package cryptography
 
 fun main() {
-    println("Hello World!")
+    while(true) {
+        println("Task (hide, show, exit):")
+        when(readLine()!!) {
+            "hide" -> println("Hiding message in image.")
+            "show" -> println("Obtaining message from image.")
+            "exit" -> {
+                println("Bye!")
+                return
+            }
+            else -> println("Wrong task: [input String]")
+        }
+    }
 }
 
